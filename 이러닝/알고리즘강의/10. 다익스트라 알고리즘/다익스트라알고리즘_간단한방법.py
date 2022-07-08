40fd1cef6debbbee37a38df80a856c94c1784346
KiiiimDong <qazz234@naver.com>
2022-07-08 22:10:29 +0900

    다익스트라 알고리즘

---
+++ 이러닝/알고리즘강의/10. 다익스트라 알고리즘/다익스트라알고리즘_간단한방법.py
@@ -0,0 +1,49 @@
+from math import dist
+import sys
+
+from multimethod import distance
+input = sys.stdin.readline
+INF = int(1e9) # 무한을 의미하는 10억
+
+n, m = map(int, input().split())
+
+start = int(input())
+
+graph = [[] for i in range(n+1)]
+
+visited = [False]*(n+1)
+
+for _ in range(m):
+    a, b, c = map(int, input().split())
+    graph[a].append((b, c))
+
+def get_smallest_node():
+    min_value = INF
+    index = 0
+    for i in range(1, n+1):
+        if distance[i] < min_value and not visited[i]:
+            min_value = distance[i]
+            index =1
+    return index
+
+def dijkstra(start):
+    distance[start] = 0
+    visited[start] = True
+    for j in graph[start]:
+        distance[j[0]] = j[i]
+    for i in range(n-1):
+        now = get_smallest_node()
+        visited[now] = True
+        for j in graph[now]:
+            cost = distance[now]+j[1]
+            if cost < distance[j[0]]:
+                distance[j[0]] = cost
+
+dijkstra(start)
+
+for i in range(1, n+1):
+    if distance[i] == INF:
+        print("INFINITY")
+    else:
+        print(distance[i])
+        
