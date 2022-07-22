f602bacb57836314a06973555ba9000e25c81114
KiiiimDong <qazz234@naver.com>
2022-07-22 11:48:07 +0900

    이진탐색

---
+++ 이러닝/알고리즘강의/20. 이진탐색 정렬된데이터에서 빠르게 데이터 찾기/정렬된 배열에서 특정수의 개수 구하기.py
@@ -0,0 +1,17 @@
+# O(logN)의 시간복잡도로 설계해야 시간초과 판정을 안받는다.
+from bisect import bisect_left, bisect_right
+
+def count_by_range(array, left_value, right_value):
+    right_index = bisect_right(array, right_value)
+    left_index = bisect_left(array, left_value)
+    return right_index - left_index
+
+n, x = map(int, input().split())
+array = list(map(int, input().split()))
+
+count = count_by_range(array, x, x)
+
+if count == 0:
+    print(-1)
+else:
+    print(count)
