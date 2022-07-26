de764ac9d1aa5741276ff4e5cb5b13f6e6329798
KiiiimDong <qazz234@naver.com>
2022-07-26 21:48:16 +0900

    구현

---
+++ 이러닝/알고리즘강의/24. 구현/시각.py
@@ -0,0 +1,11 @@
+# 86400가지를 모두 검사하는 완전탐색(brute forcing)
+
+h = int(input())
+
+count = 0
+for i in range(h+1):
+    for j in range(60):
+        for k in range(60):
+            if '3' in str(i) + str(j) + str(k):
+                count += 1
+print(count)
